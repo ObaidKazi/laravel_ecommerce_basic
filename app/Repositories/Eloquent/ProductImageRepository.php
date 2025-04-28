d507c932698844ced3cfbdf02691b0a21bd69d8d
Obaid Kazi <31682298+ObaidKazi@users.noreply.github.com>
2025-04-28 14:05:48 +0530

    Update ProductImageRepository.php

--- app/Repositories/Eloquent/ProductImageRepository.php
+++ app/Repositories/Eloquent/ProductImageRepository.php
@@ -26,16 +26,8 @@ class ProductImageRepository implements ProductImageRepositoryInterface
 
     public function getByProduct(Product $product)
     {
-        return $product->images()->orderBy('display_order')->get();
+        return $product->images()->get();
     }
 
-    public function setPrimary(ProductImage $productImage): bool
-    {
-        // First, set all images for this product as non-primary
-        ProductImage::where('product_id', $productImage->product_id)
-            ->update(['is_primary' => false]);
-        
-        // Then set this image as primary
-        return $productImage->update(['is_primary' => true]);
-    }
+    
 }
